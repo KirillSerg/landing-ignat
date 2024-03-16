cdb98124ee4913f68b8a3da3db20bb0d828977de
KirillSerg <kirill.sergienko1989@gmail.com>
2024-03-16 13:34:07 +0200

    feat: implement routing; create notFound page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./pages/notFound/NotFound";
+import Home from "./pages/home/Home";
+
 const App = () => {
-  return <>Hola amigo!</>;
+  return (
+    <>
+      {/* <Header /> */}
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/contacts" element={<Home />} /> */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
+      {/* <Footer /> */}
+    </>
+  );
 };
 
 export default App;
